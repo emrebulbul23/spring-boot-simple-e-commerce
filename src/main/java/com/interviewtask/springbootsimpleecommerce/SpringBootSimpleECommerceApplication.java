9bb8732239b1b117cb7d9308cb82fca814be217e
emrebulbul23 <emrebulbul23@gmail.com>
2020-09-03 19:49:36 +0300

    Initial commit

---
+++ src/main/java/com/interviewtask/springbootsimpleecommerce/SpringBootSimpleECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.interviewtask.springbootsimpleecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interviewtask/springbootsimpleecommerce/SpringBootSimpleECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewtask.springbootsimpleecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSimpleECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
